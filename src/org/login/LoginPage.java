4c102aaf0eb0e46846c318cc34d35721b1f51ae1
ajai133 <ajai133@gmail.com>
2022-06-17 17:57:07 +0530

    Facebook login

---
+++ src/org/login/LoginPage.java
@@ -0,0 +1,30 @@
+package org.login;
+
+public class LoginPage {
+
+	private void userName() {
+		System.out.println("Please enet your username");
+
+	}
+	private void passWord() {
+		
+		System.out.println("Please enter your password");
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	
+		
+		LoginPage l = new LoginPage();
+		l.userName();
+		l.passWord();
+		
+		
+		
+		
+	}
+	
+}
